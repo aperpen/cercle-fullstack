50c6464e8e05f62d74bd31db0ce37721c83b90ac
andres.perez.pena <andres.perez.pena@cercle.ai>
2025-09-09 16:10:48 +0200

    add css module to patients page

--- cercle-frontend/src/app/patients/page.jsx
+++ cercle-frontend/src/app/patients/page.jsx
@@ -1,10 +1,11 @@
 "use client";
+import classes from './patients.module.css'
 
 export default function PatientsTable() {
   return (
     <>
       <h1 className="text-5xl font-extrabold dark:text-white">List of patients</h1>
-      <table></table>
+      <table className={classes.table}></table>
     </>
   );
 }
